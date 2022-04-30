686023a3d8c3164187e940a322fa810040cfd370
repl.it user <replituser@example.com>
2022-04-30 22:28:53 +0500

    total change

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from datetime import date
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
