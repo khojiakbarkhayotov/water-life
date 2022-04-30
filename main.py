69875c1fc64249515b8175d49e87ee9cddb3360d
repl.it user <replituser@example.com>
2022-04-30 21:12:28 +0500

    configure admin1

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ db.create_all()
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if current_user.id != 1 or current_user.email == "inogamov2002u@gmail.com":
+        if current_user.id != 1 or current_user.email != "inogamov2002u@gmail.com":
             return abort(403)
         return f(*args, **kwargs)
     return decorated_function
